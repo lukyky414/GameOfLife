af242b82e73b6e54775fbed37463aba51bfa9155
Lucas SCHWAB <luucka.e@gmail.com>
2019-12-21 11:20:03 +0100

    Multiplication massive, block parallele

--- main.cu
+++ main.cu
@@ -1,43 +1,75 @@
 #include <stdio.h>
+#include <time.h>
+
+//Nombre de block device
+#define N 67108864
 
 //les fonctions avec __global__ seront execute sur la CG (device)
-__global__ void add(int* a, int* b, int* c){
-    *c = *a + *b;
+__global__ void mult(int* a, int* b, int* c){
+    //blockIdx.x précise le numéro du block actuel
+    c[blockIdx.x] = a[blockIdx.x] * b[blockIdx.x];
+}
+
+void random_ints(int* tab, int n){
+    int i;
+    srand (time (NULL));
+
+    for(i=0; i<n; i++)
+        tab[i] = rand();
 }
 
 //Le reste est compile avec le compilateur de base genre gcc
 int main(void) {
-    int size = sizeof(int);
+    int size = N * sizeof(int);
+    int i, tmp;
+    clock_t time1, time2;
 
     //Variables présente sur le processeur (host)
-    int h_a, h_b, h_c;
+    int *h_a, *h_b, *h_c;
 
-    //Pointeurs pour la device memory
+    //Pointeurs pour le device memory
     int *d_a, *d_b, *d_c;
+
     
     //Alloue la mémoire device
-    cudaMalloc((void **) &d_a, size);
-    cudaMalloc((void **) &d_b, size);
-    cudaMalloc((void **) &d_c, size);
+    printf("Allocation Device\n");
+    cudaMalloc((void**) &d_a, size);
+    cudaMalloc((void**) &d_b, size);
+    cudaMalloc((void**) &d_c, size);
 
-    //Valeurs de base
-    h_a = 1687;
-    h_b = 35148;
+    //Alloue la mémoire host
+    printf("Allocation Host\n");
+    h_a = (int*) malloc (size); random_ints(h_a,N);
+    h_b = (int*) malloc (size); random_ints(h_b,N);
+    h_c = (int*) malloc (size);
 
     //Copie les valeurs dans la device memory
-    cudaMemcpy(d_a, &h_a, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, &h_b, size, cudaMemcpyHostToDevice);
+    printf("Copie:\n");
+    cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice);
 
     //Calcul de somme en parallèle
-    add<<<1,1>>>(d_a, d_b, d_c);
+    //le N précise d'executer N blocs en parallele
+    printf("Calcul en parallele:");
+    time1 = clock();
+    mult<<<N,1>>>(d_a, d_b, d_c);
+    time2 = clock();
+    printf("%d\n", time2-time1);
+
+    printf("Calcul en local:");
+    time1 = clock();
+    for(i=0; i<N; i++)
+        h_c[i] = h_a[i] * h_b[i];
+    time2 = clock();
+    printf("%d\n", time2-time1);
 
     //Récupération des donnes du device vers le host
-    cudaMemcpy(&h_c, d_c, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost);
 
-    //Désallocation de la device memory
+    //Désallocation du device memory
     cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
-    
-    printf("Calcul en parallele:\n%d + %d = %d\n", h_a, h_b, h_c);
 
+    //Désallocation du host memory
+    free(h_a); free(h_b); free(h_c);
     return 0;
 }
