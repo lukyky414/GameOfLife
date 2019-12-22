fa67bb26546bb951232237e05d606719e7bc2688
Lucas SCHWAB <luucka.e@gmail.com>
2019-12-22 11:09:54 +0100

    Adapter avec max threads / block

--- main.cu
+++ main.cu
@@ -1,76 +1,91 @@
 #include <stdio.h>
 #include <time.h>
 
-#define T_LIGNE 50
-#define T_COLON 5
+#define NB_COLONNE 256
+#define NB_LIGNE 256
+#define NB_THREAD 1024
 
 //Epoque calcule sur le device. Un appel a cette fonction par pixel
 __global__ void epoque(char* in, char* out){
-    int x = threadIdx.x;
+    int first_x = threadIdx.x;
+    int x;
     int y = blockIdx.x;
 
     //Pour éviter de refaire les multiplication
-    int t_2ligne = T_LIGNE * 2;
-    int out_pos = x + y * T_LIGNE;
+    int t_2ligne = NB_COLONNE * 2;
+    int out_line = y * NB_COLONNE;
 
     int somme = 0;
 
-    __shared__ char ligne[T_LIGNE * 3];
+    __shared__ char ligne[NB_COLONNE * 3];
 
     //Recopier la ligne du dessus, la ligne actuel, et celle du dessous pour les calculs.
-    ligne[x] = ( y>=0 ? in[x + (y-1)*T_LIGNE] : (char)0);
-    ligne[x + T_LIGNE] = in[x + y*T_LIGNE];
-    ligne[x + t_2ligne] = (y<T_COLON? in[x + (y+1)*T_LIGNE] : (char)0);
+    x = first_x;
+    while(x < NB_COLONNE){
+        ligne[x] = ( y>=0 ? in[x + (y-1)*NB_COLONNE] : (char)0);
+        ligne[x + NB_COLONNE] = in[x + y*NB_COLONNE];
+        ligne[x + t_2ligne] = (y<NB_LIGNE? in[x + (y+1)*NB_COLONNE] : (char)0);
+
+        x += NB_THREAD;
+    }
 
     //Attendre que le block ait finis de recopier les lignes
     __syncthreads();
 
-    if(x > 1)
-        somme += ligne[x-1] + ligne[x-1 + T_LIGNE] + ligne[x-1 + t_2ligne];
-    somme += ligne[x] + ligne[x + t_2ligne];
-    if(x < T_LIGNE-1)
-        somme += ligne[x+1] + ligne[x+1 + T_LIGNE] + ligne[x+1 + t_2ligne];
-    
-    //case vivante
-    if(ligne[x + T_LIGNE] == 1){
-        if(somme == 3 || somme == 2)
-            out[out_pos] = (char)1;
-        else
-            out[out_pos] = (char)0;
-    }
-    //case morte
-    else{
-        if(somme == 3)
-            out[out_pos] = (char)1;
-        else
-            out[out_pos] = (char)0;
+    x = first_x;
+    while(x < NB_COLONNE){
+        if(x > 1)
+            somme += ligne[x-1] + ligne[x-1 + NB_COLONNE] + ligne[x-1 + t_2ligne];
+        somme += ligne[x] + ligne[x + t_2ligne];
+        if(x < NB_COLONNE-1)
+            somme += ligne[x+1] + ligne[x+1 + NB_COLONNE] + ligne[x+1 + t_2ligne];
+
+        //case vivante
+        if(ligne[x + NB_COLONNE] == 1){
+            if(somme == 3 || somme == 2)
+                out[x + out_line] = (char)1;
+            else
+                out[x + out_line] = (char)0;
+        }
+        //case morte
+        else{
+            if(somme == 3)
+                out[x + out_line] = (char)1;
+            else
+                out[x + out_line] = (char)0;
+        }
+
+        x += NB_THREAD;
     }
 }
 
 //Générer une carte de départ
-void random_ints(char* tab, int n){
+void random_map(char* map, int n){
     int i;
     srand (time (NULL));
 
     for(i=0; i<n; i++)
-        tab[i] = rand()%2;
+        map[i] = rand()%2;
 }
 
 void affichage(char* map){
     int i, j;
-    for(j=0; j<T_COLON; j++){
-        for(i=0; i<T_LIGNE; i++)
-            printf((map[i + j*T_LIGNE]?"#":" "));
+    for(j=0; j<NB_LIGNE; j++){
+        for(i=0; i<NB_COLONNE; i++)
+            printf((map[i + j*NB_COLONNE]?"#":" "));
         printf("\n");
     }
 }
 
 //Le reste est compile avec le compilateur de base genre gcc
 int main(void) {
-    int N = T_LIGNE * T_COLON;
+    //Informations sur la map
+    int N = NB_COLONNE * NB_LIGNE;
     int size = N * sizeof(int);
-    int k, state = 0;
-    clock_t t;
+
+    //Variables de boucles
+    int k=0, state = 0;
+    clock_t t, t_1;
 
     //Variables présente sur le processeur (host)
     char *map;
@@ -85,8 +100,8 @@ int main(void) {
 
     //Alloue la mémoire host
     printf("Allocation Host\n");
-    map = (char*) malloc (size); random_ints(map,N);
-    affichage(map);
+    map = (char*) malloc (size); random_map(map,N);
+    //affichage(map);
 
     //Copie les valeurs dans la device memory
     printf("Copie:\n");
@@ -94,29 +109,22 @@ int main(void) {
 
     cudaDeviceSynchronize();
 
-    printf("Epoques:\n");
-    t = clock();
-    for(k=0; k<64; k++){
+    printf("Epoques:");
+    t_1 = clock();
+    t = (clock()-t_1)/CLOCKS_PER_SEC;
+    while(t < 1){
         if(state){
             state = 0;
-            epoque<<<T_COLON,T_LIGNE>>>(map2, map1);
+            epoque<<<NB_LIGNE,NB_THREAD>>>(map2, map1);
         }
         else{
             state = 1;
-            epoque<<<T_COLON,T_LIGNE>>>(map1, map2);
+            epoque<<<NB_LIGNE,NB_THREAD>>>(map1, map2);
         }
-
-        if(state)
-            cudaMemcpy(map, map2, size, cudaMemcpyDeviceToHost);
-        else
-            cudaMemcpy(map, map1, size, cudaMemcpyDeviceToHost);
-        
-        cudaDeviceSynchronize();
-        affichage(map);
-        printf("-----\n");
+        t = (clock()-t_1)/CLOCKS_PER_SEC;
+        k++;
     }
-    t = clock()-t;
-    printf("%.3f\n", (double)t/CLOCKS_PER_SEC);
+    printf("%d in %.3fs\n", k, (double)t);
 
     cudaDeviceSynchronize();
 
@@ -127,7 +135,7 @@ int main(void) {
         cudaMemcpy(map, map1, size, cudaMemcpyDeviceToHost);
     
     cudaDeviceSynchronize();
-    affichage(map);
+    //affichage(map);
 
     //Désallocation du device memory
     cudaFree(map1); cudaFree(map2);
